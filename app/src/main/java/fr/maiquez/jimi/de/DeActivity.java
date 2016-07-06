cdcc9f8a40b7990ea251cc057b71d99ff7d3d627
ItsJimi <jimi.maiquez@gmail.com>
2016-07-06 16:20:09 +0200

    First commit

---
+++ app/src/main/java/fr/maiquez/jimi/de/DeActivity.java
@@ -0,0 +1,40 @@
+package fr.maiquez.jimi.de;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class DeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_de);
+
+        launch(null);
+    }
+
+    public void launch(View view) {
+        Intent intent = getIntent();
+        String string_nbr = intent.getStringExtra(MainActivity.STRING_NBR);
+        int nbr = (isNumber(string_nbr)) ? Integer.parseInt(string_nbr) : 6;
+        int randNbr = (int) ((Math.random() * nbr) + 1);
+        String de = String.valueOf(randNbr);
+        TextView number;
+        number = (TextView) findViewById(R.id.number);
+        if (number != null) {
+            number.setText(de);
+        }
+    }
+
+    public static boolean isNumber(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException nfe) {}
+        return false;
+    }
+}
---
+++ app/src/main/java/fr/maiquez/jimi/de/MainActivity.java
@@ -0,0 +1,28 @@
+package fr.maiquez.jimi.de;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String STRING_NBR = "fr.maiquez.jimi.de.NBR";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void submit(View view) {
+        Intent newIntent = new Intent(this, DeActivity.class);
+        EditText number = (EditText) findViewById(R.id.number);
+        String string_nbr = number.getText().toString();
+        newIntent.putExtra(STRING_NBR, string_nbr);
+        startActivity(newIntent);
+    }
+}
